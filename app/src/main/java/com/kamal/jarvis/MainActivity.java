d227d7429f9731b441c435153b0a9e608ef0415d
kamalf522 <khallafkamal26@gmail.com>
2026-09-15 00:04:50 +0100

    Create java/com/kamal/jarvis/MainActivity.java

---
+++ app/src/main/java/com/kamal/jarvis/MainActivity.java
@@ -0,0 +1,39 @@
+package com.kamal.jarvis;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.graphics.Color;
+import android.view.Gravity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        layout.setGravity(Gravity.CENTER);
+        layout.setPadding(40, 40, 40, 40);
+        layout.setBackgroundColor(Color.rgb(11, 16, 32));
+
+        TextView title = new TextView(this);
+        title.setText("KAMAL JARVIS");
+        title.setTextColor(Color.WHITE);
+        title.setTextSize(30);
+        title.setGravity(Gravity.CENTER);
+
+        TextView status = new TextView(this);
+        status.setText("\nJARVIS ONLINE\n\nReady.");
+        status.setTextColor(Color.LTGRAY);
+        status.setTextSize(18);
+        status.setGravity(Gravity.CENTER);
+
+        layout.addView(title);
+        layout.addView(status);
+
+        setContentView(layout);
+    }
+}
